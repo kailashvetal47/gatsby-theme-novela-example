64aa7c0633005b267ce021f78e82da883f2c4c97
Kailash Vetal <Kailash.VETAL@3ds.com>
2020-03-04 09:26:31 +0100

    App : Removes local change

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,6 @@ module.exports = {
           format: 'dddd D MMMM YYYY', // string, defaults to "MM/DD/YYYY" - pass in any acceptable date-and-time format
           utc: false, // boolean, defaults to false - output time as UTC or not, following date-and-time API
         },
-        locale: 'en', // string, defaults to null, which date-and-time defaults as "en" - whether to localize the date or not, can use any available date-and-time localization
       },
     },
   ],
